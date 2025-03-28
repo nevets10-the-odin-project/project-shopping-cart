5b7764cdb09b7e7d9a998199381d55a9c75c2e5c
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-27 18:07:05 -0700

    Add navigation to ShoppingCart

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navigation from "./Navigation";
 
 export default function ShoppingCart() {
 	return (
 		<div>
-			ShoppingCart
-			<Link to="/">home</Link>
+			<Navigation />
+			<h1>ShoppingCart</h1>
 		</div>
 	);
 }

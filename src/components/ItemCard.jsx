c8432611f04cf26c695d9b5e62194f7159276715
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-29 12:30:16 -0700

    Create ItemCard and add to Shop

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ItemCard({ item }) {
+	return <div>{item.title}</div>;
+}
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "./Navigation";
+import ItemCard from "./ItemCard";
 
 export default function Shop() {
+	const [items, setItems] = useState([]);
+
+	console.log(items);
+	useEffect(() => {
+		fetch("https://fakestoreapi.com/products")
+			.then((response) => response.json())
+			.then((data) => setItems(data));
+	}, []);
 	return (
 		<>
 			<Navigation />
 			<h1>Shop</h1>
+			{items.length > 0 &&
+				items.map((item) => <ItemCard id={item.id} item={item} />)}
 		</>
 	);
 }

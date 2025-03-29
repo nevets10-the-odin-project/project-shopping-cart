71db65f21b7d2ea5df1d3ce1e10ffb01a8931548
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-29 12:39:41 -0700

    Populate ItemCard, add styles for items and list

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import styles from "../styles/itemCard.module.css";
 
 export default function ItemCard({ item }) {
-	return <div>{item.title}</div>;
+	return (
+		<div className={styles.itemCard}>
+			<img src={item.image} alt="" />
+			<h2>{item.title}</h2>
+			<p>{item.category}</p>
+			<p>{item.price}</p>
+			<p>{item.description}</p>
+		</div>
+	);
 }
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
+import styles from "../styles/shop.module.css";
 import Navigation from "./Navigation";
 import ItemCard from "./ItemCard";
 
 export default function Shop() {
 	const [items, setItems] = useState([]);
 
-	console.log(items);
 	useEffect(() => {
 		fetch("https://fakestoreapi.com/products")
 			.then((response) => response.json())
@@ -14,9 +14,10 @@ export default function Shop() {
 	return (
 		<>
 			<Navigation />
-			<h1>Shop</h1>
-			{items.length > 0 &&
-				items.map((item) => <ItemCard key={item.id} item={item} />)}
+			<div className={styles.itemList}>
+				{items.length > 0 &&
+					items.map((item) => <ItemCard key={item.id} item={item} />)}
+			</div>
 		</>
 	);
 }

ef4b5373f4fce5fc67e0a6892c4223082213a2af
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-29 14:03:03 -0700

    Add cart link and styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import styles from "../styles/navigation.module.css";
-import { Link } from "react-router-dom";
+import { Link, useHref } from "react-router-dom";
+import Icon from "@mdi/react";
+import { mdiCartVariant } from "@mdi/js";
 
 export default function Navigation() {
+	const href = useHref();
+
 	return (
 		<div className={styles.nav}>
 			<Link to="/">Home</Link>
 			<Link to="/shop">Shop</Link>
+
+			{href.includes("shop") && (
+				<Link to="/shop/cart">
+					<Icon path={mdiCartVariant} title="cart" size={1.4} />
+				</Link>
+			)}
 		</div>
 	);
 }

8ca92ab1848f3b99d192240db5982bcf290ccfda
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-29 12:04:21 -0700

    Populate Home and add styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import "./App.css";
+import { Link } from "react-router-dom";
+import styles from "./App.module.css";
 import Navigation from "./components/Navigation";
 
 function App() {
 	return (
 		<>
 			<Navigation />
-			<h1>HOME</h1>
+			<div className={styles.home}>
+				<h1>Home Page</h1>
+				<p>
+					Welcome to the home page! This project is a part of the React course on{" "}
+					<a href="https://www.theodinproject.com/">The Odin Project</a>. The project
+					demonstrates the use of concepts in the React ecosystem. Including: React
+					Router, fetching data in React, and CSS Module. Visit the{" "}
+					<Link to="shop">shop</Link> page for a demonstration!
+				</p>
+			</div>
 		</>
 	);
 }

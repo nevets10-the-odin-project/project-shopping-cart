51e6d55fbd094330aab1c3db89a9e30363d72c99
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-29 13:32:51 -0700

    Adjust ItemCard and styles

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -5,10 +5,11 @@ export default function ItemCard({ item }) {
 	return (
 		<div className={styles.itemCard}>
 			<img src={item.image} alt="" />
-			<h2>{item.title}</h2>
-			<p>{item.category}</p>
-			<p>{item.price}</p>
-			<p>{item.description}</p>
+			<h2 className={styles.title}>{item.title}</h2>
+			<div>
+				<p>${item.price}</p>
+				<p className={styles.description}>{item.description}</p>
+			</div>
 		</div>
 	);
 }

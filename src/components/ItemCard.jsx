8a04e600d0274ac29cf66057b00534f9a3ade541
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-29 14:24:24 -0700

    Add insert item into cart functionality

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "../styles/itemCard.module.css";
+import Icon from "@mdi/react";
+import { mdiCartArrowDown } from "@mdi/js";
 
-export default function ItemCard({ item }) {
+export default function ItemCard({ item, handleAddToCart }) {
 	return (
 		<div className={styles.itemCard}>
 			<img src={item.image} alt="" />
 			<h2 className={styles.title}>{item.title}</h2>
 			<div>
-				<p>${item.price}</p>
+				<div className={styles.priceBuy}>
+					<p>${item.price}</p>
+					<button type="button" onClick={handleAddToCart}>
+						<Icon path={mdiCartArrowDown} size={1} />
+					</button>
+				</div>
 				<p className={styles.description}>{item.description}</p>
 			</div>
 		</div>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -5,6 +5,16 @@ import ItemCard from "./ItemCard";
 
 export default function Shop() {
 	const [items, setItems] = useState([]);
+	const [cart, setCart] = useState([]);
+
+	function handleAddToCart(item) {
+		const existingItem = cart.find((cartItem) => cartItem.id === item.id);
+		console.log(existingItem);
+		if (existingItem) return;
+
+		const newCart = [...cart, item];
+		setCart(newCart);
+	}
 
 	useEffect(() => {
 		fetch("https://fakestoreapi.com/products")
@@ -13,10 +23,16 @@ export default function Shop() {
 	}, []);
 	return (
 		<>
-			<Navigation />
+			<Navigation cartCount={cart.length} />
 			<div className={styles.itemList}>
 				{items.length > 0 &&
-					items.map((item) => <ItemCard key={item.id} item={item} />)}
+					items.map((item) => (
+						<ItemCard
+							key={item.id}
+							item={item}
+							handleAddToCart={() => handleAddToCart({ id: item.id, count: 1 })}
+						/>
+					))}
 			</div>
 		</>
 	);

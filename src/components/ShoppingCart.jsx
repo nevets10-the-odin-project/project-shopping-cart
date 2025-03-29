be5eeea9925e0d08e351cee8299440a3df82d3b9
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-29 15:36:33 -0700

    Add total to ShoppingCart

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -3,11 +3,13 @@ import CartItemCard from "./CartItemCard";
 import styles from "../styles/cart.module.css";
 
 export default function ShoppingCart({ cart }) {
+	const total = cart.reduce((acc, item) => (acc += item.price * item.count), 0);
 	return (
 		<div className={styles.cart}>
 			{cart.map((item) => (
 				<CartItemCard key={item.id} item={item} />
 			))}
+			<p className={styles.total}>Total: ${total}</p>
 		</div>
 	);
 }
